69346c201f562521043a900ff9288b0824cec82a
Filippo Bragato <filo.bragato@gmail.com>
2023-03-06 15:51:02 +0100

    added packet parsing (missing 2, 11, 15, 16, 19)

--- PythonAPI/util/log_parser.py
+++ PythonAPI/util/log_parser.py
@@ -20,6 +20,10 @@ def parse_string(byte_string:bytes):
 
     return string_data, string_end
 
+def parse_FVector(bytedata:bytes):
+    vector = {}
+    vector["x"],vector["y"],vector["z"] = struct.unpack("<fff", bytedata)
+    return vector
 # class LogHeader(Structure):
 #     _fields_ = [
 #         ("version",c_uint16),
@@ -54,6 +58,76 @@ def parse_infoheader(bytedata:bytes):
 
     return header_info, header_end
 
+
+def parse_position_record(bytedata:bytes):
+    pos = {}
+    pos["id"],pos["x"],pos["y"],pos["z"],pos["pitch"],pos["yaw"],pos["roll"] = struct.unpack("<Iffffff", bytedata)
+    return pos
+
+def parse_collision_record(bytedata:bytes):
+    coll = {}
+    coll["recording_id"],coll["actor_1_id"],coll["actor_2_id"],coll["is_hero_1"],coll["is_hero_2"] = struct.unpack("<III??", bytedata)
+    return coll
+
+def parse_state_record(bytedata:bytes):
+    state = {}
+    state["id"], state["is_frozen"], state["elapsed_time"], state["state"] = struct.unpack("<I?fc", bytedata)
+    return state
+
+def parse_animVehicle_record(bytedata:bytes):
+    animVehicle = {}
+    animVehicle["id"], animVehicle["Steering"], animVehicle["Throttle"], animVehicle["Brake"] , animVehicle["bHandbrake"] , animVehicle["Gear"] = struct.unpack("<Ifff?i", bytedata)
+    return animVehicle
+
+def parse_animWalker_record(bytedata:bytes):
+    animWalker = {}
+    animWalker["id"], animWalker["Speed"] = struct.unpack("<If", bytedata)
+    return animWalker
+
+
+def parse_kinematic_record(bytedata:bytes):
+    kinematic = {}
+    kinematic["id"] = struct.unpack("<If", bytedata[:2])[0]
+    kinematic["Speed"] = parse_FVector(bytedata[2:14])
+    kinematic["AngularVelocity"] = parse_FVector(bytedata[14:26])
+    return kinematic
+
+def parse_boundingbox_record(bytedata:bytes):
+    bounding_box = {}
+    bounding_box["id"] = struct.unpack("<If", bytedata[:2])[0]
+    bounding_box["Origin"] = parse_FVector(bytedata[2:14])
+    bounding_box["Estention"] = parse_FVector(bytedata[14:26])
+    return bounding_box
+
+def parse_delevent_record(bytedata:bytes):
+    del_event = {}
+    del_event["DatabaseId"] = struct.unpack("<I", bytedata)[0]
+    return(del_event)
+
+def parse_eventParent_record(bytedata:bytes):
+    parent_event = {}
+    parent_event["DatabaseId"],parent_event["DatabaseParentId"] = struct.unpack("<II", bytedata)
+    return parent_event
+
+def parse_VehicleLight_record(bytedata:bytes):
+    vehicleLight = {}
+    vehicleLight["DatabaseId"],vehicleLight["State"] = struct.unpack("<II", bytedata)
+    return vehicleLight
+
+def parse_SceneLight_record(bytedata:bytes):
+    sceneLight = {}
+    # TODO
+    return sceneLight
+
+def parse_multiple_records(packet:dict, data_bytes:bytes, record_name:str, record_length:int, f_parse_single):
+    packet["n_record"] = struct.unpack("<H", data_bytes[0:2])[0]
+    packet[record_name] = []
+    for i in range(packet["n_record"]):
+        start = 2 + i * record_length
+        end = 2 + i * record_length + record_length
+        packet[record_name].append(f_parse_single(data_bytes[start:end]))
+
+
 def parse_packet(bytedata:bytes):
     packet = {}
     
@@ -61,14 +135,74 @@ def parse_packet(bytedata:bytes):
     packet_id, data_size = struct.unpack('<BI',bytedata[:header_end])
     packet_end = header_end + data_size
 
+    packet["id"] = packet_id
+
     data_bytes = bytedata[header_end:packet_end]
     
-    if packet_id==0:
-        # Frame Start
+    if packet_id==0: # Frame Start
         packet["frame_id"],packet["duration"],packet["elapsed"] = struct.unpack('<Qdd',data_bytes)
-    elif packet_id==2:
-        # Add
-        total, id = struct.unpack('<HI',data_bytes[:6])[0]
+
+    elif packet_id==1: #Frame End
+        pass
+
+    elif packet_id==2: #Add event
+        pass #TODO
+
+    elif packet_id==3: #Del event
+        parse_multiple_records(packet, data_bytes, "EventsDel", 4, parse_delevent_record)
+
+    elif packet_id==4: #EventParent
+        parse_multiple_records(packet, data_bytes, "EventParent", 8, parse_eventParent_record)
+
+    elif packet_id==5: # Collsion
+        parse_multiple_records(packet, data_bytes, "collisions", 14, parse_collision_record)
+
+    elif packet_id==6: # Position
+        parse_multiple_records(packet, data_bytes, "positions", 28, parse_position_record)
+
+    elif packet_id==7: # state
+        parse_multiple_records(packet, data_bytes, "states", 10, parse_state_record)
+        
+    elif packet_id==8: # anim vehicle
+        parse_multiple_records(packet, data_bytes, "AnimVehicle", 21, parse_animVehicle_record)
+
+    elif packet_id==9: # anim walkers
+        parse_multiple_records(packet, data_bytes, "AnimWalkers", 8, parse_animWalker_record)
+
+    elif packet_id==10: #VehicleLight
+        parse_multiple_records(packet, data_bytes, "VehicleLight", 8, parse_VehicleLight_record)
+
+    elif packet_id==11: #SceneLight
+        #parse_multiple_records(packet, data_bytes, "SceneLight", 0, parse_SceneLight_record)
+        pass
+        
+    elif packet_id==12: #Kinematics
+        parse_multiple_records(packet, data_bytes, "Kinematics", 28, parse_kinematic_record)
+        
+    elif packet_id==13: #BoundingBox
+        parse_multiple_records(packet, data_bytes, "BoundingBoxes", 28, parse_boundingbox_record)
+
+    elif packet_id==14: #PlatformTime
+        packet['time'] = struct.unpack('<d',data_bytes)
+
+    elif packet_id==15:
+        pass
+
+    elif packet_id==16:
+        pass
+
+    elif packet_id==17: #TriggerVolume
+        parse_multiple_records(packet, data_bytes, "TriggerVolume", 26, parse_boundingbox_record)
+        
+    elif packet_id==18:
+        packet['FrameCounter'] = struct.unpack('<Q',data_bytes)
+
+    elif packet_id==19:
+        pass
+
+    elif packet_id==20:
+        packet['time'] = struct.unpack('<d',data_bytes)
+        
     return packet, packet_end
 
 host = 'localhost'
